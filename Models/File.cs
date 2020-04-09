779ee3d4962fbb336aa85f2f050e8275c987c164
stinakarlsson <56968340+stinakarlsson@users.noreply.github.com>
2020-04-09 16:11:54 +0200

    .

--- Models/File.cs
+++ Models/File.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -12,6 +13,9 @@ namespace ScrumProject.Models
         [Key]
         public string Root { get; set; }
 
-        public virtual Post PostID { get; set; }
+        [ForeignKey("Post")]
+        public int PostID { get; set; }
+        public Post Post { get; set; }
+
     }
 }
--- Models/Post.cs
+++ Models/Post.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -15,5 +16,10 @@ namespace ScrumProject.Models
         public DateTime PostDateTime { get; set; }
         public virtual User UserID { get; set; }
         public virtual Category CategoryName { get; set; }
+
+        [InverseProperty("Post")]
+        public IList<File> PostFile { get; set; }
+
+
     }
 }
