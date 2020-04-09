b84456644f3d9dbaa6fff0cf10709db6e9bc36de
Elinore Hagman <55893776+ElinoreHagman@users.noreply.github.com>
2020-04-09 16:20:35 +0200

    Revert "."
    This reverts commit 779ee3d4962fbb336aa85f2f050e8275c987c164.

--- Models/File.cs
+++ Models/File.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -13,9 +12,6 @@ namespace ScrumProject.Models
         [Key]
         public string Root { get; set; }
 
-        [ForeignKey("Post")]
-        public int PostID { get; set; }
-        public Post Post { get; set; }
-
+        public virtual Post PostID { get; set; }
     }
 }
--- Models/Post.cs
+++ Models/Post.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -16,10 +15,5 @@ namespace ScrumProject.Models
         public DateTime PostDateTime { get; set; }
         public virtual User UserID { get; set; }
         public virtual Category CategoryName { get; set; }
-
-        [InverseProperty("Post")]
-        public IList<File> PostFile { get; set; }
-
-
     }
 }
