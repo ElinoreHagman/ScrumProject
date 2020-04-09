fab8e65f3c6c4fecc0a5f3937c9fe69445b464de
Mattias Bolander <55517764+TiasB@users.noreply.github.com>
2020-04-09 11:02:29 +0200

    DbSets included for Entities

--- Models/BlogDbContext.cs
+++ Models/BlogDbContext.cs
@@ -9,6 +9,12 @@ namespace ScrumProject.Models
     public class BlogDbContext : DbContext
     {
         public DbSet<Profile> Profiles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Meeting> Meetings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<File> Files { get; set; }
+        public DbSet<Invite> Invites { get; set; }
 
         public BlogDbContext() : base("BlogDb") { }
     }
