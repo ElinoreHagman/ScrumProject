4e547a0bd46efeb803e783bdc8ee97726dfcd9e1
Mattias Bolander <55517764+TiasB@users.noreply.github.com>
2020-04-13 13:16:14 +0200

    InviteTableCreated
    Invite table created with DbContext but no AddForm created.

---
+++ Controllers/InviteController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ScrumProject.Models;
+
+namespace ScrumProject.Controllers
+{
+    public class InviteController : Controller
+    {
+        // GET: Invite
+        public ActionResult Index()
+        {
+            var ctx = new BlogDbContext();
+            var viewModel = new InviteIndexViewModel
+            {
+                Invites = ctx.Invites.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
