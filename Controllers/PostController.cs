6ceac2827781aceffb9ea88ab902f5229f26db05
juliacarnehagen <56970551+juliacarnehagen@users.noreply.github.com>
2020-04-14 16:48:19 +0200

    Lagt in metod som sparar ner nytt blogginlägg!

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace ScrumProject.Models
 {
     public class PostController : Controller
     {
         // GET: Post
-        public ActionResult Index()
+        public ActionResult CreatePost()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePost(Post model){
+
+            var ctx = new BlogDbContext();
+            var post = new Post
+            {
+                ProfileID = User.Identity.GetUserId(),
+                Title = model.Title,
+                Content = model.Content,
+                PublishedWall = model.PublishedWall,
+                PostDateTime = DateTime.Now,
+                CategoryID = 1, //Denna måste vi ändra senare
+            };
+            ctx.Posts.Add(post);
+            ctx.SaveChanges();
+
+            return View(post);
         }
     }
 }
