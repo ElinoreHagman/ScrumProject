9b52aa6cdf67b06fa96e8612b306fa6e88be2499
stinakarlsson <56968340+stinakarlsson@users.noreply.github.com>
2020-04-14 09:35:31 +0200

    Posts viewmodel & controller

---
+++ Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ScrumProject.Models;
+
+namespace ScrumProject.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            var ctx =  new BlogDbContext();
+            var viewModel = new PostIndexViewModel
+            {
+                Posts = ctx.Posts.ToList()
+
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Models/PostIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScrumProject.Models
+{
+    public class PostIndexViewModel
+    {
+        public List<Post> Posts { get; set; }
+    }
+}
