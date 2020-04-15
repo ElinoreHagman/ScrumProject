0ecb644b3a1e38b93c229ed4d70fd55a40f9ecd4
majahard <56968986+majahard@users.noreply.github.com>
2020-04-15 11:08:53 +0200

    Skapat funktion för att visa inlägg
    Det är baserat på branchen för Create post

---
+++ Controllers/WallController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ScrumProject.Models;
+
+namespace ScrumProject.Controllers
+{
+    public class WallController : Controller
+    {
+        // GET: Wall
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FormalWall()
+        {
+            var blogDB = new BlogDbContext();
+            var viewModel = new PostIndexViewModel
+            {
+                Posts = blogDB.Posts.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
