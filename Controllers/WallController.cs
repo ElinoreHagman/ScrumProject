42f6dd682cf0f8c685d099f7248c98ab6fae2f15
stinakarlsson <56968340+stinakarlsson@users.noreply.github.com>
2020-04-16 14:35:26 +0200

    DropDown

--- Controllers/WallController.cs
+++ Controllers/WallController.cs
@@ -9,33 +9,46 @@ namespace ScrumProject.Controllers
 {
     public class WallController : Controller
     {
-        // GET: Wall
+        //GET: Wall
         public ActionResult Index()
         {
             return View();
         }
 
-        //public ActionResult FormalWall()
-        //{
-        //    var blogDB = new BlogDbContext();
-        //    var viewModel = new PostIndexViewModel
-        //    {
-        //        Posts = blogDB.Posts.ToList()
-        //    };
-        //    return View(viewModel);
-        //}
-
         public ActionResult FormalWall()
         {
             var blogDB = new BlogDbContext();
-            
+            var viewModel = new PostIndexViewModel
+            {
+                Posts = blogDB.Posts.ToList()
+            };
+            return View(viewModel);
+        }
+
+
+        //BlogDbContext db = new BlogDbContext();
+
+        //public ActionResult Test()
+
+        //{
+        //    ViewBag.Message = "hej";
+        //    return View();
+        //    //ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "Name");
+        //    //return RedirectToAction("FormalWall");
+        //}
+
+        [HttpPost]
+        public ActionResult _sortCategory()
+        {
+            var blogDB = new BlogDbContext();
+
             {
                 var categories = blogDB.Categories.ToList();
                 var viewModel = new PostIndexViewModel { Categories = categories };
                 return View(viewModel);
             }
 
-           
+
         }
 
     }
