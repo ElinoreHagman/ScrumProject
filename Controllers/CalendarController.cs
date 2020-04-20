53c762d8ec0e10ad120caebcdfbbbc82051e6add
stinakarlsson <56968340+stinakarlsson@users.noreply.github.com>
2020-04-20 11:08:14 +0200

    Skapat formulär för möte
    Det går bara att bjuda in en person och det finns ingen datetime.

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -17,7 +17,14 @@ namespace ScrumProject.Controllers
             viewmodel.Meetings = ctx.Meetings.ToList();
             viewmodel.ProfilesToMeetings = ctx.ProfilesToMeetings.ToList();
 
+            ViewBag.people = ctx.Profiles.ToList();
+
             return View(viewmodel);
+        }
+
+        public ActionResult SendInvite(MeetingIndexViewModel model, string dropdownMenu)
+        {
+            return View();
         }
     }
 }
---
+++ Models/MeetingIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScrumProject.Models
+{
+    public class MeetingIndexViewModel
+    {
+        public string MeetingName { get; set; }
+        public DateTime MeetingDate { get; set; }
+        public string Invited { get; set; }
+        public IList<Profile> profiles { get; set; }
+    }
+}
